f90f77742009902ddac744aed1487e1485505e9e
Martin David Valentino <martin.david.siagian@gmail.com>
2017-03-20 16:09:46 +1100

    setup new java project

---
+++ src/com/ezshare/client/Main.java
@@ -0,0 +1,8 @@
+package com.ezshare.client;
+
+/**
+ * Created by mvalentino on 20/3/17.
+ */
+public class Main {
+
+}
---
+++ src/com/ezshare/server/Main.java
@@ -0,0 +1,8 @@
+package com.ezshare.server;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
