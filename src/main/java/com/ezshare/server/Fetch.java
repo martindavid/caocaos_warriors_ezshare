762097bd8f11410e711f6e6a4c60b497592c1780
Haoran Sun <254664427@qq.com>
2017-04-17 16:40:45 +1000

    Put a break after client receive a response

---
+++ src/main/java/com/ezshare/server/Fetch.java
@@ -0,0 +1,17 @@
+package com.ezshare.server;
+
+import com.ezshare.Resource;
+
+public class Fetch {
+	private Resource resource;
+	
+	public Fetch(Resource resource) {
+		this.resource = resource;
+	}
+	
+	
+	
+	
+	
+
+}
